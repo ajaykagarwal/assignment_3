2cea778fedc701531879f7fef509577414debd5d
AJAY AGARWAL <65657895+ak-dotcom@users.noreply.github.com>
2020-06-14 13:47:17 +0530

    Create 3.js

---
+++ 3.js
@@ -0,0 +1,13 @@
+(function () {
+
+var names = ["Ajay", "Joy", "janman", "Jay", "sourabh", "skit", "Jaipur", "Rajasthan", "USA", "India"];
+for (var i = 0; i < names.length; i++) {
+var firstLetter = names[i].charAt(0).toLowerCase();
+if (firstLetter === 'j') {
+    byeSpeaker.sayGoodbye(names[i]);
+  } else {
+    helloSpeaker.sayHello(names[i]);
+  }
+}
+
+})();
